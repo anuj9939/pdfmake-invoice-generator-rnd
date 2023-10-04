da13edb96c412d0ff77ab6ff980f7170d3ce3262
anuj9939 <anujaaryankr@gmail.com>
2023-10-04 18:32:53 +0530

    added header footer test text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,207 +4,216 @@ import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 class Product {
-  name: string = '';
-  price: number = 0;
-  qty: number = 0;
+    name: string = '';
+    price: number = 0;
+    qty: number = 0;
 }
 class Invoice {
-  customerName: string = 'Abhishek Jaiswal Kumar';
-  address: string = 'Maharajganj Patna City, Patna, Bihar';
-  contactNo: number = 9852465112;
-  email: string = 'abhishekjaiswal@gmail.com';
+    customerName: string = 'Abhishek Jaiswal Kumar';
+    address: string = 'Maharajganj Patna City, Patna, Bihar';
+    contactNo: number = 9852465112;
+    email: string = 'abhishekjaiswal@gmail.com';
 
-  products: Product[] = [];
-  additionalDetails: string = '';
+    products: Product[] = [];
+    additionalDetails: string = '';
 
-  constructor() {
-    // Initially one empty product row we will show
-    for (let i = 1; i <= 30; i++)
-      this.products.push({
-        name: 'Product ' + i,
-        qty: i,
-        price: i * 2,
-      });
-  }
+    constructor() {
+        // Initially one empty product row we will show
+        for (let i = 1; i <= 30; i++)
+            this.products.push({
+                name: 'Product ' + i,
+                qty: i,
+                price: i * 2,
+            });
+    }
 }
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  invoice = new Invoice();
-  constructor() {
-    (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
-  }
-
-  generatePDF(action = 'open') {
-    let docDefinition: any = {
-      header: {
-        margin: 8,
-        columns: [
-          {
-            table: {
-              widths: ['100%'],
-              height: ['20%'],
-              body: [
-                [
-                  {
-                    text: 'ELECTRONIC SHOP',
-                    fontSize: 16,
-                    alignment: 'center',
-                    color: '#047886',
-                  },
-                ],
-                [
-                  {
-                    text: 'INVOICE',
-                    fontSize: 20,
-                    bold: true,
-                    alignment: 'center',
-                    decoration: 'underline',
-                    color: 'skyblue',
-                  },
-                ],
-              ],
-            },
-            layout: 'noBorders',
-          },
-          // {
-          //   text: 'ELECTRONIC SHOP',
-          //   fontSize: 16,
-          //   alignment: 'center',
-          //   color: '#047886',
-          // },
-          // {
-          //   text: 'INVOICE',
-          //   fontSize: 20,
-          //   bold: true,
-          //   alignment: 'center',
-          //   decoration: 'underline',
-          //   color: 'skyblue',
-          // },
-          // {
-          //   text: 'Customer Details',
-          //   style: 'sectionHeader',
-          // },
-        ],
-      },
-      content: [
-        {
-          text: 'ELECTRONIC SHOP',
-          fontSize: 16,
-          alignment: 'center',
-          color: '#047886',
-        },
-        {
-          text: 'INVOICE',
-          fontSize: 20,
-          bold: true,
-          alignment: 'center',
-          decoration: 'underline',
-          color: 'skyblue',
-        },
-        {
-          text: 'Customer Details',
-          style: 'sectionHeader',
-        },
-        {
-          columns: [
-            [
-              {
-                text: this.invoice.customerName,
-                bold: true,
-              },
-              { text: this.invoice.address },
-              { text: this.invoice.email },
-              { text: this.invoice.contactNo },
-            ],
-            [
-              {
-                text: `Date: ${new Date().toLocaleString()}`,
-                alignment: 'right',
-              },
-              {
-                text: `Bill No : ${(Math.random() * 1000).toFixed(0)}`,
-                alignment: 'right',
-              },
-            ],
-          ],
-        },
-        {
-          text: 'Order Details',
-          style: 'sectionHeader',
-        },
-        {
-          table: {
-            headerRows: 1,
-            widths: ['*', 'auto', 'auto', 'auto'],
-            body: [
-              ['Product', 'Price', 'Quantity', 'Amount'],
-              ...this.invoice.products.map((p) => [
-                p.name,
-                p.price,
-                p.qty,
-                (p.price * p.qty).toFixed(2),
-              ]),
-              [
-                { text: 'Total Amount', colSpan: 3 },
-                {},
-                {},
-                this.invoice.products
-                  .reduce((sum, p) => sum + p.qty * p.price, 0)
-                  .toFixed(2),
-              ],
-            ],
-          },
-        },
-        {
-          text: 'Additional Details',
-          style: 'sectionHeader',
-        },
-        {
-          text: this.invoice.additionalDetails,
-          margin: [0, 0, 0, 15],
-        },
-        {
-          columns: [
-            [{ qr: `${this.invoice.customerName}`, fit: '50' }],
-            [{ text: 'Signature', alignment: 'right', italics: true }],
-          ],
-        },
-        {
-          text: 'Terms and Conditions',
-          style: 'sectionHeader',
-        },
-        {
-          ul: [
-            'Order can be return in max 10 days.',
-            'Warrenty of the product will be subject to the manufacturer terms and conditions.',
-            'This is system generated invoice.',
-          ],
-        },
-      ],
-      styles: {
-        sectionHeader: {
-          bold: true,
-          decoration: 'underline',
-          fontSize: 14,
-          margin: [0, 15, 0, 15],
-        },
-      },
-    };
-
-    if (action === 'download') {
-      pdfMake.createPdf(docDefinition).download();
-    } else if (action === 'print') {
-      pdfMake.createPdf(docDefinition).print();
-    } else {
-      pdfMake.createPdf(docDefinition).open();
+    invoice = new Invoice();
+    constructor() {
+        (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
     }
-  }
 
-  addProduct() {
-    this.invoice.products.push(new Product());
-  }
+    generatePDF(action = 'open') {
+        let docDefinition: any = {
+            // Page size and margins
+            pageSize: 'A4',
+            pageMargins: [40, 80, 40, 80],
+
+            // Content of the PDF
+            content: [
+                // Table content
+                {
+                    table: {
+                        widths: ['*', '*'],
+                        body: [
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            ['Customer Name', 'ANUJ'],
+                            ['Customer Email', 'customerDetails.email'],
+                            // Add more rows as needed
+                        ],
+                    },
+                    layout: 'noBorders', // Removes table borders
+                    margin: [40, 0],
+                },
+            ],
+
+            // Page header and footer
+            header: (currentPage: any, pageCount: any, pageSize: any) => {
+                // Define multiple rows of data in the header
+                let headerData = [
+                    {
+                        text: 'Header Row 1',
+                        style: 'header',
+                        margin: [40, 10, 40, 0],
+                    },
+                    {
+                        text: 'Header Row 2',
+                        style: 'header',
+                        margin: [40, 0, 40, 0],
+                    },
+                    {
+                        text: 'Header Row 3',
+                        style: 'header',
+                        margin: [40, 0, 40, 0],
+                    },
+                ];
+
+                return headerData;
+            },
+            footer: (currentPage: any, pageCount: any) => {
+                return [
+                    {
+                        text: 'Footer Row 1',
+                        style: 'footer',
+                        margin: [40, 0, 40, 0],
+                    },
+                    {
+                        text: 'Footer Row 2',
+                        style: 'footer',
+                        margin: [40, 0, 40, 0],
+                    },
+                    {
+                        text: 'Footer Row 3',
+                        style: 'footer',
+                        margin: [40, 0, 40, 0],
+                    },
+                    ,
+                    {
+                        text: 'Page ' + currentPage.toString() + ' of ' + pageCount,
+                        alignment: 'center',
+                    },
+                ];
+            },
+        };
+
+        if (action === 'download') {
+            pdfMake.createPdf(docDefinition).download();
+        } else if (action === 'print') {
+            pdfMake.createPdf(docDefinition).print();
+        } else {
+            pdfMake.createPdf(docDefinition).open();
+        }
+    }
+
+    addProduct() {
+        this.invoice.products.push(new Product());
+    }
 }
